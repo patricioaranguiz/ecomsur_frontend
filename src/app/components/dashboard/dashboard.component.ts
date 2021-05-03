56397cca2a785156164e9043e7d99f520fdc741d
Patricio Aránguiz <patricio.aranguiz@catena.cl>
2021-05-03 12:27:11 -0400

    Se agrega gráfico.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
+import {Chart, registerables} from 'chart.js';
+import {DashboardService} from '../../services/dashboard.service';
+
+Chart.register(...registerables);
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
+    canvas: any;
+    ctx: any;
 
-  constructor() { }
+    chart = [];
 
-  ngOnInit(): void {
-  }
+    @ViewChild('myChart') myChart: any;
+
+    constructor(private dashboardSvc: DashboardService) {
+    }
+
+
+    ngAfterViewInit(): void {
+        this.canvas = this.myChart.nativeElement;
+        this.ctx = this.canvas.getContext('2d');
+        const labels = [];
+        const data = [];
+        this.dashboardSvc.getCountMemberOfGroup()
+            .subscribe(response => {
+                response.map(res => {
+                    labels.push(res.group.toUpperCase());
+                    data.push(res.count);
+                });
+                // tslint:disable-next-line:no-unused-expression
+                new Chart(this.ctx, {
+                    type: 'pie',
+                    data: {
+                        labels,
+                        datasets: [{
+                            label: '# of Votes',
+                            data,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        plugins: {
+                            title: {
+                                display: true,
+                                text: 'Cantidad de usuarios por Grupo'
+                            }
+                        }
+                    }
+                });
+            });
+    }
+
+    ngOnInit(): void {
+
+    }
 
 }
