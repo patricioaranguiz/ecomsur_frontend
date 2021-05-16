9fb36aecdcb7f629d13505d5cbc4b906f5502243
Patricio Aránguiz <patricio.aranguiz@catena.cl>
2021-05-15 21:31:49 -0400

    Implementación de dashboard para la cantidad de usuarios por departamento.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -16,6 +16,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     chart = [];
 
     @ViewChild('myChart') myChart: any;
+    @ViewChild('countMemberOfDepartment') countMemberOfDepartment: any;
 
     constructor(private dashboardSvc: DashboardService) {
     }
@@ -24,10 +25,11 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     ngAfterViewInit(): void {
         this.canvas = this.myChart.nativeElement;
         this.ctx = this.canvas.getContext('2d');
-        const labels = [];
-        const data = [];
+
         this.dashboardSvc.getCountMemberOfGroup()
             .subscribe(response => {
+                const labels = [];
+                const data = [];
                 response.map(res => {
                     labels.push(res.group.toUpperCase());
                     data.push(res.count);
@@ -69,9 +71,58 @@ export class DashboardComponent implements OnInit, AfterViewInit {
                     }
                 });
             });
+
+        const count = this.countMemberOfDepartment.nativeElement.getContext('2d');
+
+        this.dashboardSvc.getCountMemberOfDepartament()
+            .subscribe(response => {
+                const labels = [];
+                const data = [];
+                response.map(res => {
+                    labels.push(res.name.toUpperCase());
+                    data.push(res.count);
+                });
+                // tslint:disable-next-line:no-unused-expression
+                new Chart(count, {
+                    type: 'pie',
+                    data: {
+                        labels,
+                        datasets: [{
+                            label: '# of Votes',
+                            data,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        plugins: {
+                            title: {
+                                display: true,
+                                text: 'Cantidad de usuarios por Departamento'
+                            }
+                        }
+                    }
+                });
+            });
     }
 
     ngOnInit(): void {
+
 
     }
 
